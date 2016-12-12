30aa4afe7cd03511241132f0cbc84889fbff3cea
Volodya <prokopiv.v@gmail.com>
2016-12-12 11:41:24 +0000

    add new file

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+        var $menu = $("#menu");
+
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+                $menu.removeClass("default").addClass("fixed");
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.removeClass("fixed").addClass("default");
+            }
+        });//scroll
+    });
+    
+    $(function() {
+    
+    $('a.navmenu').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+    
+});
+
